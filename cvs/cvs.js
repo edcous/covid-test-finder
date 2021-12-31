90918cbef7e7c14f9dfa234b23cd945180c0d7db
edcous <49954272+edcous@users.noreply.github.com>
2021-12-30 20:43:05 -0600

    Update cvs.js

--- cvs/cvs.js
+++ cvs/cvs.js
@@ -25,13 +25,14 @@ async function cvs(){
             const res = await axios.post('https://www.cvs.com/RETAGPV3/OnlineShopService/V2/getSKUInventoryAndPrice', {"request":{"header":{"lineOfBusiness":"RETAIL","appName":"CVS_WEB","apiKey":"a2ff75c6-2da7-4299-929d-d670d827ab4a","channelName":"WEB","deviceToken":"d9708df38d23192e","deviceType":"DESKTOP","responseFormat":"JSON","securityType":"apiKey","source":"CVS_WEB","type":"retleg"}},"skuId":[config[i]["sku"]],"pageName":"PLP"})
             const stock = res.data.response.getSKUInventoryAndPrice.skuInfo[0].stockLevel != 0
             const browserType = playwright.webkit
-            const browser = await browserType.launch();
+            const browser = await browserType.launch({headless:false});
             const context = await browser.newContext();
             const page = await context.newPage();
             await page.goto("https://www.cvs.com/shop/" + config[i]["url"]);
             await timer(3000);
             const price = await page.innerText('[class="css-901oao r-1khnkhu r-1jn44m2 r-3i2nvb r-vw2c0b r-1b7u577"]', 'query')
             const query = { store: "CVS", storeID: sku }
+            await browser.close();
             Stock.count(query, function (err, count){
                 if(count == 0){
                     Stock.create({store: "CVS", storeID: sku, isInStock: stock, pricePer: price.replace('$','')})
