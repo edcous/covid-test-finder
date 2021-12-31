afbcfb767417dbb56a8d09ddc2e518ac888a5345
edcous <49954272+edcous@users.noreply.github.com>
2021-12-30 18:10:36 -0600

    Update target.js

--- target/target.js
+++ target/target.js
@@ -24,9 +24,9 @@ async function target(){
         const date = new Date().toISOString()
         console.log(sku)
         const res = await axios.get('https://redsky.target.com/redsky_aggregations/v1/web/pdp_fulfillment_v1?key=ff457966e64d5e877fdbad070f276d18ecec4a01&tcin=' + sku + '&store_id=2255&store_positions_store_id=2255&has_store_positions_store_id=true&zip=63664&state=MO&latitude=37.930&longitude=-90.780&pricing_store_id=2255&has_pricing_store_id=true&is_bot=false')
-        const price = await axios.get('https://redsky.target.com/redsky_aggregations/v1/web/pdp_client_v1?key=ff457966e64d5e877fdbad070f276d18ecec4a01&tcin=' + sku + '&store_id=2255&pricing_store_id=2255&has_financing_options=true&visitor_id=017D3B56C6050201BAB2A842960390FF&has_size_context=true&latitude=37.930&longitude=-90.780&state=MO&zip=63664')
+        const price1 = await axios.get('https://redsky.target.com/redsky_aggregations/v1/web/pdp_client_v1?key=ff457966e64d5e877fdbad070f276d18ecec4a01&tcin=' + sku + '&store_id=2255&pricing_store_id=2255&has_financing_options=true&visitor_id=017D3B56C6050201BAB2A842960390FF&has_size_context=true&latitude=37.930&longitude=-90.780&state=MO&zip=63664')
         const stock = res.data.data.product.fulfillment.shipping_options.availability_status != "OUT_OF_STOCK"
-        const price = parseFloat(res.data.data.product.price.current_retail)
+        const price = parseFloat(price1.data.data.product.price.current_retail)
         const query = { store: "Target", storeID: sku };
         Stock.count(query, function (err, count){
             if(count == 0){
