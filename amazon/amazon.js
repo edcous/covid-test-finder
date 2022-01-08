8ab99840430fcba63b7a82cfbba4c9bfa32a063a
edcous <49954272+edcous@users.noreply.github.com>
2022-01-07 20:25:44 -0600

    fix amzn

--- amazon/amazon.js
+++ amazon/amazon.js
@@ -7,6 +7,9 @@ const Stock = require('../models/stock.js')
 const connection = require('../config/db.config.js');
 connection.once('open', () => console.log('DB Connected'))
 connection.on('error', () => console.log('Error with DB'))
+const puppeteer = require('puppeteer-extra')
+const StealthPlugin = require('puppeteer-extra-plugin-stealth')
+puppeteer.use(StealthPlugin())
 
 const config = JSON.parse(fs.readFileSync('./amazon/tests.json', 'utf8'))
 require('dotenv').config()
@@ -18,43 +21,42 @@ if(!stores.includes('amazon')){
 
 async function amazon() {
   for (var i = 0; i < config.length; i++) {
-    const browserType = playwright.firefox
-    const browser = await browserType.launch({});
-    const context = await browser.newContext();
-    const page = await context.newPage();
-    await page.goto("https://www.amazon.com/dp/" + config[i]["id"]);
-    await timer(5000);
-    const stock = await page.$('[id="add-to-cart-button"]') !== null
-    var price;
-    if(stock){
-        price = await page.innerText('[class="a-offscreen"]')
-    }
-    const date = new Date().toISOString()
-    const query = { store: "Amazon", storeID: config[i]["id"] };
-    await page.screenshot({ path: './' + config[i]["id"] + ".png" });
-    Stock.count(query, function (err, count){
-      if(count == 0){
-        Stock.create({store: "Amazon", storeID: config[i]["id"], isInStock: stock, lastUpdated: date, pricePer: parseInt(price), purchaseLink: "https://www.amazon.com/dp/" + config[i]["id"]})
-      }
-      else{
-            if(!stock){
-                Stock.findOneAndUpdate(query, {isInStock: stock, lastUpdated: date}, {upsert: false}, function(err, doc) {});
-            }
-            else{
-                Stock.findOneAndUpdate(query, {isInStock: stock, lastUpdated: date, pricePer: price.replace('$','')}, {upsert: false}, function(err, doc) {});
-            }
-            console.log(price)
-      }
-    })
-    await browser.close();
-    const embed = new MessageBuilder()
-    .setTitle('COVID Test Stock Update')
-    .setDescription('Stock on item :' + config[i]["name"] + " is " + stock)
-    .setColor('#00b0f4')
-    .setTimestamp();
-    hook.send(embed);
-    hook.sendFile('./' + config[i]["id"] + ".png")
-    await timer(120000);
+      const browser = await puppeteer.launch({ headless: true })
+      const page = await browser.newPage()
+      await page.setViewport({ width: 1000, height: 600 })
+      const cookiesString = await fs.readFileSync('./cookies.json');
+      var cookies = JSON.parse(cookiesString);
+      await page.setCookie(...cookies);
+      await page.goto("https://www.amazon.com/dp/" + config[i]["id"]);
+      await timer(5000);
+      const stock = await page.$('[id="add-to-cart-button"]') !== null
+      const date = new Date().toISOString()
+      const query = { store: "Amazon", storeID: config[i]["id"] };
+      await page.screenshot({ path: './' + config[i]["id"] + ".png" });
+      Stock.count(query, function (err, count){
+        if(count == 0){
+          Stock.create({store: "Amazon", storeID: config[i]["id"], isInStock: stock, lastUpdated: date, pricePer: parseInt(price), purchaseLink: "https://www.amazon.com/dp/" + config[i]["id"]})
+        }
+        else{
+              if(!stock){
+                  Stock.findOneAndUpdate(query, {isInStock: stock, lastUpdated: date}, {upsert: false}, function(err, doc) {});
+              }
+              else{
+                  Stock.findOneAndUpdate(query, {isInStock: stock, lastUpdated: date}, {upsert: false}, function(err, doc) {});
+              }
+        }
+      })
+      const embed = new MessageBuilder()
+      .setTitle('COVID Test Stock Update')
+      .setDescription('Stock on item :' + config[i]["name"] + " is " + stock)
+      .setColor('#00b0f4')
+      .setTimestamp();
+      hook.send(embed);
+      hook.sendFile('./' + config[i]["id"] + ".png")
+      cookies = await page.cookies();
+      await fs.writeFileSync('./cookies.json', JSON.stringify(cookies, null, 2));
+      await browser.close();
+      await timer(120000);
   }
 }
 
