a60f7e04a9f81404e985a82db14df555b3b158a8
edcous <49954272+edcous@users.noreply.github.com>
2022-01-12 10:41:48 -0600

    Update walmart.js

--- walmart/walmart.js
+++ walmart/walmart.js
@@ -8,7 +8,7 @@ const Stock = require('../models/stock.js')
 const connection = require('../config/db.config.js');
 connection.once('open', () => console.log('DB Connected'))
 connection.on('error', () => console.log('Error with DB'))
-const config = JSON.parse(fs.readFileSync('./walmart/tests.json', 'utf8'))
+var config = JSON.parse(fs.readFileSync('./walmart/tests.json', 'utf8'))
 
 require('dotenv').config()
 const stores = process.env.storesToRun.toString().toLowerCase();
@@ -17,42 +17,82 @@ if(!stores.includes('ongo')){
     process.exit()
 }
 
+if(stores.includes('walmart')){
+  config = config[3]
+  console.log(config)
+}
 
 async function walmart() {
-    for (var i = 0; i < config.length; i++) {
+    if(config.length=1){
       const browserType = playwright.webkit
-      const browser = await browserType.launch({});
+      const browser = await browserType.launch({headless:false});
       const context = await browser.newContext();
       const page = await context.newPage();
-      await page.goto("https://www.walmart.com/ip/" + config[i]["upc"]);
-      await page.screenshot({ path: './' + config[i]["upc"].split('/')[1] + ".png" });
+      await page.goto("https://www.walmart.com/ip/" + config["upc"]);
+      await page.screenshot({ path: './' + config["upc"].split('/')[1] + ".png" });
       const stock = await page.$("text='Add to cart'") !== null && (await page.$("text='Shipping, '") !== null || await page.$("text='Free shipping, '") !== null)
       const price = await page.innerText('[itemprop="price"]', 'query')
       console.log(price)
       console.log(stock)
       const date = new Date().toISOString()
-      const query = { store: "Walmart", storeID: config[i]["upc"].split('/')[1] };
+      const query = { store: "Walmart", storeID: config["upc"].split('/')[1] };
       Stock.count(query, function (err, count){
         if(count == 0){
-          Stock.create({store: "Walmart", storeID: config[i]["upc"].split('/')[1], isInStock: stock, lastUpdated: date, pricePer: price.replace('$',''), purchaseLink: "https://www.walmart.com/ip/" + config[i]["upc"]})
+          Stock.create({store: "Walmart", storeID: config["upc"].split('/')[1], isInStock: stock, lastUpdated: date, pricePer: price.replace('$',''), purchaseLink: "https://www.walmart.com/ip/" + config["upc"]})
         }
         else{
-          Stock.findOneAndUpdate(query, {isInStock: stock, lastUpdated: date, pricePer: price.replace('$',''), purchaseLink: "https://www.walmart.com/ip/" + config[i]["upc"]}, {upsert: false}, function(err, doc) {});
+          Stock.findOneAndUpdate(query, {isInStock: stock, lastUpdated: date, pricePer: price.replace('$',''), purchaseLink: "https://www.walmart.com/ip/" + config["upc"]}, {upsert: false}, function(err, doc) {});
         }
       })
       await browser.close();
       const embed = new MessageBuilder()
       .setTitle('COVID Test Stock Update')
-      .setDescription('Stock on item :' + config[i]["brand"] + " is " + stock)
+      .setDescription('Stock on item :' + config["brand"] + " is " + stock)
       .setColor('#00b0f4')
       .setTimestamp();
       hook.send(embed);
-      hook.sendFile('./' + config[i]["upc"].split('/')[1] + '.png');
-      await timer(70000)
+      hook.sendFile('./' + config["upc"].split('/')[1] + '.png');
     }
-  }
-
-  var minutes = 5, the_interval = minutes * 60 * 1000;
+    else{
+      for (var i = 0; i < config.length; i++) {
+        const browserType = playwright.webkit
+        const browser = await browserType.launch({headless:false});
+        const context = await browser.newContext();
+        const page = await context.newPage();
+        await page.goto("https://www.walmart.com/ip/" + config[i]["upc"]);
+        await page.screenshot({ path: './' + config[i]["upc"].split('/')[1] + ".png" });
+        const stock = await page.$("text='Add to cart'") !== null && (await page.$("text='Shipping, '") !== null || await page.$("text='Free shipping, '") !== null)
+        const price = await page.innerText('[itemprop="price"]', 'query')
+        console.log(price)
+        console.log(stock)
+        const date = new Date().toISOString()
+        const query = { store: "Walmart", storeID: config[i]["upc"].split('/')[1] };
+        Stock.count(query, function (err, count){
+          if(count == 0){
+            Stock.create({store: "Walmart", storeID: config[i]["upc"].split('/')[1], isInStock: stock, lastUpdated: date, pricePer: price.replace('$',''), purchaseLink: "https://www.walmart.com/ip/" + config[i]["upc"]})
+          }
+          else{
+            Stock.findOneAndUpdate(query, {isInStock: stock, lastUpdated: date, pricePer: price.replace('$',''), purchaseLink: "https://www.walmart.com/ip/" + config[i]["upc"]}, {upsert: false}, function(err, doc) {});
+          }
+        })
+        await browser.close();
+        const embed = new MessageBuilder()
+        .setTitle('COVID Test Stock Update')
+        .setDescription('Stock on item :' + config[i]["brand"] + " is " + stock)
+        .setColor('#00b0f4')
+        .setTimestamp();
+        hook.send(embed);
+        hook.sendFile('./' + config[i]["upc"].split('/')[1] + '.png');
+        await timer(70000)
+      }
+  
+    }
+}
+var minutes = 6
+if(process.env.minutes){
+  minutes = parseInt(process.env.minutes)
+}
+var the_interval = minutes * 60 * 1000;
 setInterval(function() {
   walmart()
 }, the_interval);
