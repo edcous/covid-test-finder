196ee2a458d8669e0d410a8a4a7a6d4fa4db8cf6
edcous <49954272+edcous@users.noreply.github.com>
2022-02-15 07:29:17 -0600

    Update cvs.js

--- cvs/cvs.js
+++ cvs/cvs.js
@@ -4,7 +4,7 @@ const fs = require('fs')
 const Stock = require('../models/stock.js')
 const connection = require('../config/db.config.js');
 const config = JSON.parse(fs.readFileSync('./cvs/tests.json', 'utf8'))
-var minutes = 5, the_interval = minutes * 60 * 1000;
+var minutes = 60, the_interval = minutes * 60 * 1000;
 const timer = ms => new Promise(res => setTimeout(res, ms))
 const { Webhook, MessageBuilder } = require('discord-webhook-node');
 const hook = new Webhook("https://discord.com/api/webhooks/925840857209380885/Cm0_aNfJ_gnuzssYhHJs424NiB_7RqPzR5Cg3AaDfO6qYowAHHZoLUjIqAUQtzh16iOa");
@@ -22,13 +22,16 @@ async function cvs(){
             const sku = config[i]["sku"]
             const date = new Date().toISOString()
             console.log(sku)
-            const res = await axios.post('https://www.cvs.com/RETAGPV3/OnlineShopService/V2/getSKUInventoryAndPrice', {"request":{"header":{"lineOfBusiness":"RETAIL","appName":"CVS_WEB","apiKey":"a2ff75c6-2da7-4299-929d-d670d827ab4a","channelName":"WEB","deviceToken":"d9708df38d23192e","deviceType":"DESKTOP","responseFormat":"JSON","securityType":"apiKey","source":"CVS_WEB","type":"retleg"}},"skuId":[config[i]["sku"]],"pageName":"PLP"}, { headers: {Cookie: cookie}})
-            const stock = res.data.response.getSKUInventoryAndPrice.skuInfo[0].stockLevel != 0
             const browserType = playwright.webkit
             const browser = await browserType.launch({});
             const context = await browser.newContext();
             const page = await context.newPage();
+            await page.goto("https://www.cvs.com/shop/" + config[i]["url"]);
+            await timer(3000);
+            const price = await page.innerText('[class="css-901oao r-1khnkhu r-1jn44m2 r-3i2nvb r-vw2c0b r-1b7u577"]', 'query')
+            const stock = await page.$("text='Eligible for 1-4 day shipping'") !== null
             const query = { store: "CVS", storeID: sku }
+            await page.screenshot({ path: './' + 'c-f-c' + ".png" });
             await browser.close();
             Stock.count(query, function (err, count){
                 if(count == 0){
@@ -43,8 +46,9 @@ async function cvs(){
               .setDescription('Stock on item :' + config[i]["brand"] + " is " + stock)
               .setColor('#00b0f4')
               .setTimestamp();
-              hook.send(embed);      
-              await timer(15000)
+              hook.sendFile('./' + 'c-f-c' + ".png")          
+              hook.send(embed);
+              await timer(60*1000*5)
         }
 }
 
